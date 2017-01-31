fe3e4228b36fa27ba418526cd37b0a5aadc79a69
Timothy Rasmussen <timothy.s.rasmussen@gmail.com>
2017-01-31 19:50:24 +0100

    New Update

--- TCPServer/src/TCPServer.java
+++ TCPServer/src/TCPServer.java
@@ -1,30 +1,45 @@
-
 import java.io.*;
 import java.net.*;
 
-/**
- * Created by timot on 31-01-2017.
- */
+class TCPServer {
 
-public class TCPServer {
+    public static void main(String argv[]) throws Exception {
+        // declares input and output strings for the serever
+        String clientSentence;
 
-        public static void main(String argv[]) throws Exception
-        {
-            String clientSentence;
-            String capitalizedSentence;
-            ServerSocket welcomeSocket = new ServerSocket(6789);
+        // initialises the server, at port 6789
+        ServerSocket welcomeSocket = new ServerSocket(6789);
 
-            while(true)
-            {
-                Socket connectionSocket = welcomeSocket.accept();
-                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-                clientSentence = inFromClient.readLine();
-                System.out.println("Received: " + clientSentence);
-                capitalizedSentence = clientSentence.toUpperCase() + '\n';
-                outToClient.writeBytes(capitalizedSentence);
+        while (true) {
+            // opens the socket with the client
+            Socket connectionSocket = welcomeSocket.accept();
+
+            // opens a buffer with the socket, to read the clients input
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+            // opens a buffer with the socket, to print from server to client
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+            // reads input from the client and save it as clientSentence
+            clientSentence = inFromClient.readLine();
+            System.out.println("Received: " + clientSentence);
+
+            // writes the input back to the client char for char
+            for (int i = 0; i < clientSentence.length(); i++) {
+                char c = clientSentence.charAt(i);
+                outToClient.writeByte(c);
             }
+
+
+            // writes the input back to the client ten times
+            for (int i = 0; i < 10; i++) {
+                outToClient.writeBytes(clientSentence);
+
+            }
+
+            outToClient.writeByte('\n');
+
         }
+
+    }
 }
-
-
